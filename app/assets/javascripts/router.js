e5aeea6c3435dc279a94d275e5a24e0bd1dcb135
Scott Weisman <scott@launchpadlab.com>
2014-08-25 23:58:46 -0500

    turned off hash in routes

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,5 +1,12 @@
 // For more information see: http://emberjs.com/guides/routing/
 
+// Turn off # in url
+EmbergolfRails.Router.reopen({
+  location: 'history'
+});
+
+
 EmbergolfRails.Router.map(function() {
 	this.route('about');
 });
+
