be8797bc2c28ccf4b06c61a3d38071aa145f33d5
Scott Weisman <scott@launchpadlab.com>
2014-09-01 22:49:23 -0500

    added search

--- app/assets/javascripts/controllers/courses.js
+++ app/assets/javascripts/controllers/courses.js
@@ -1,3 +1,15 @@
 App.CoursesController = Ember.ArrayController.extend({
-  sortProperties: ['name']
+
+  sortProperties: ['name'],
+
+  courses: function() {
+    return this.get('search') ? this.get('searchedCourses') : this
+  }.property('search', 'searchedCourses'),
+
+  searchedCourses: function() {
+    var search = this.get('search').toLowerCase()
+    return this.filter(function(course) {
+      return course.get('name').toLowerCase().indexOf(search) != -1
+    })
+  }.property('search', 'this.@each.name')
 })
